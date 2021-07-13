f29ba5077bb066693680dd5983a11f057eaf4d8a
Yash-Singh1 <saiansh2525@gmail.com>
2021-07-13 14:38:48 -0700

    Change layout and add thermometer and progress rings

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,12 +3,15 @@ import { Button, InputGroup, FormControl } from 'react-bootstrap';
 import formatDate from '../helpers/formatDate';
 import { useDispatch, useSelector } from 'react-redux';
 import 'weathericons/css/weather-icons.min.css';
-import processWeather from '../helpers/processWeather';
 import LOCALES from '../data/localization';
 import filterData from '../helpers/filterData';
 import parseHeaders from 'parse-headers';
 import patchQuestionIcon from 'bootstrap-icons/icons/patch-question.svg';
 import { setDate } from '../actions';
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import ProgressProvider from './ProgressProvider';
+import 'react-circular-progressbar/dist/styles.css';
+import Thermometer from 'react-thermometer-component';
 
 function Dashboard({ query }) {
   let [tempDestroy, setTempDestroy] = useState(false);
@@ -23,6 +26,7 @@ function Dashboard({ query }) {
     )
   );
   const language = useSelector((state) => state.language);
+  const darkMode = useSelector((state) => state.darkMode);
 
   const dispatch = useDispatch();
 
@@ -88,11 +92,144 @@ function Dashboard({ query }) {
           <span>{LOCALES.noData[language]}</span>
         </div>
       ) : (
-        <div id='preview-icons' className='row' data-aos='fade-up'>
-          {['Morning', 'Afternoon', 'Evening', 'Night'].map(
-            processWeather(data, language)
-          )}
-        </div>
+        <>
+          <div className='container-fluid' data-aos='fade-up'>
+            <div className='row'>
+              <div className='col-3'>
+                <Thermometer
+                  theme={darkMode ? 'dark' : 'light'}
+                  value={data.temperature.slice(0, -1)}
+                  max='50'
+                  steps='3'
+                  format='°C'
+                  size='large'
+                  height='300'
+                />
+              </div>
+              <div className='col-9'>
+                <div id='preview-icons' className='row'>
+                  {['Morning', 'Afternoon', 'Evening', 'Night'].map(
+                    (stage, index) => {
+                      const windy =
+                        data.wind !== 'Unknown' &&
+                        parseFloat(data.wind.split(' at ')[1].split(' ')[0]) >=
+                          20;
+                      const raining =
+                        parseFloat(data['chance of rain'].slice(0, -1)) >= 60;
+                      const foggy =
+                        parseFloat(data['humidity'].slice(0, -1)) >= 95;
+                      return (
+                        <div key={index} className='col-3 mx-auto'>
+                          <span className='text-nowrap'>
+                            {LOCALES[stage][language]}
+                          </span>
+                          <br />
+                          <i
+                            className={`weather-icon wi ${
+                              data.condition === 'Sunny'
+                                ? 'wi-' +
+                                  (stage === 'Morning' || stage === 'Afternoon'
+                                    ? 'day-sunny'
+                                    : 'night-clear')
+                                : data.condition === 'Cloudy'
+                                ? raining
+                                  ? 'wi-' +
+                                    (stage === 'Morning' ||
+                                    stage === 'Afternoon'
+                                      ? ''
+                                      : 'night-alt-') +
+                                    'rain' +
+                                    (windy ? '-wind' : '')
+                                  : 'wi-' + (foggy ? 'fog' : 'cloudy')
+                                : data.condition === 'Partly Cloudy'
+                                ? raining && windy
+                                  ? 'wi-' +
+                                    (stage === 'Morning' ||
+                                    stage === 'Afternoon'
+                                      ? 'day'
+                                      : 'night-alt') +
+                                    '-rain-wind'
+                                  : raining
+                                  ? 'wi-' +
+                                    (stage === 'Morning' ||
+                                    stage === 'Afternoon'
+                                      ? 'day'
+                                      : 'night-alt') +
+                                    '-rain'
+                                  : 'wi-' +
+                                    (stage === 'Morning' ||
+                                    stage === 'Afternoon'
+                                      ? 'day'
+                                      : 'night' + (foggy ? '' : '-alt')) +
+                                    (foggy ? '-fog' : '-partly-cloudy')
+                                : ''
+                            } font-size-50`}
+                          ></i>
+                          <br />
+                          <span className='text-nowrap'>
+                            {data.condition === 'Sunny'
+                              ? stage === 'Morning' || stage === 'Afternoon'
+                                ? LOCALES.sunny[language]
+                                : LOCALES.clear[language]
+                              : foggy
+                              ? LOCALES.foggy[language]
+                              : raining
+                              ? 'Raining'
+                              : windy
+                              ? 'windy'
+                              : data.condition === 'Cloudy'
+                              ? 'Cloudy'
+                              : data.condition === 'Partly Cloudy'
+                              ? 'Partly Cloudy'
+                              : ''}
+                          </span>
+                        </div>
+                      );
+                    }
+                  )}
+                </div>
+                <div className='row'>
+                  <div className='col-6'>
+                    <div
+                      style={{ width: 200, maxWidth: 'inherit' }}
+                      className='mx-auto'
+                    >
+                      <ProgressProvider
+                        valueStart={10}
+                        valueEnd={parseFloat(
+                          data['chance of rain'].slice(0, -1)
+                        )}
+                      >
+                        {(value) => (
+                          <CircularProgressbarWithChildren
+                            value={value}
+                          >{`Chance of rain: ${value}%`}</CircularProgressbarWithChildren>
+                        )}
+                      </ProgressProvider>
+                    </div>
+                  </div>
+                  <div className='col-6'>
+                    <div
+                      style={{ width: 200, maxWidth: 'inherit' }}
+                      className='mx-auto'
+                    >
+                      <ProgressProvider
+                        valueStart={10}
+                        valueEnd={parseFloat(data['humidity'].slice(0, -1))}
+                      >
+                        {(value) => (
+                          <CircularProgressbarWithChildren
+                            value={value}
+                          >{`Relative Humidity: ${value}%`}</CircularProgressbarWithChildren>
+                        )}
+                      </ProgressProvider>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
