6e9d95c6d6a46cf1c8f8b26b7587e058e02d2a7f
Yash-Singh1 <saiansh2525@gmail.com>
2021-07-17 17:25:51 -0700

    Cleanup hook and cleanup component

--- src/components/ProgressProvider.js
+++ src/components/ProgressProvider.js
@@ -2,17 +2,21 @@
  * Brought from https://codesandbox.io/s/0zk372m7l?file=/ProgressProvider.js:0-434
  */
 
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 // If you don't have a version of React that supports
 // hooks, you can use a class-based version of this
 // component in ProgressProviderUsingClass.js
 const ProgressProvider = ({ valueStart, valueEnd, children }) => {
-  const [value, setValue] = React.useState(valueStart);
-  React.useEffect(() => {
+  const [value, setValue] = useState(valueStart);
+  const [timeouts, setTimeouts] = useState([]);
+
+  useEffect(() => {
     if (value !== valueEnd) {
-      setTimeout(() => setValue(value + 1), 15);
+      setTimeouts([...timeouts, setTimeout(() => setValue(value + 1), 15)]);
     }
+
+    return () => timeouts.forEach(clearTimeout);
   }, [value]);
 
   return children(value);
